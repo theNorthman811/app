cee4851a1cbdd9dd0ccce63e7abbd203f558e868
theNorthman811 <sbel35467@gmail.com>
2023-08-08 13:22:42 +0200

    3  commit

---
+++ app.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+from flask import Flask, request, render_template
+import joblib
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Load the model
+model = joblib.load("best_model_svc.pkl")
+
+# Load the multilabel binarizer
+with open("binarizer.pkl", "rb") as file:
+    multilabel_binarizer = pickle.load(file)
+
+# Load the TfidfVectorizer
+with open("vectorizer.pkl", "rb") as file:
+    vectorizer = pickle.load(file)
+
+# Create Flask app
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index1.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    text_input = request.form["text_input"]
+
+    # Convert the text into a numerical feature vector using your pre-processing steps
+    features = pre_process_text(text_input, vectorizer)
+
+    # Make prediction
+    prediction = model.predict(features)
+
+    # Inverse multilabel binarizer
+    tags_predict = multilabel_binarizer.inverse_transform(prediction)
+
+    return render_template("index1.html", prediction_text="The predicted tags are {}".format(tags_predict))
+
+def pre_process_text(text, vectorizer):
+    # Replace this function with your own pre-processing code to convert the text input into numerical features
+
+    from texthero import preprocessing
+    import texthero as hero
+
+    dat = pd.DataFrame([text])
+
+    custom_pipeline = [preprocessing.fillna,
+                       preprocessing.lowercase,
+                       preprocessing.remove_digits,
+                       preprocessing.remove_punctuation,
+                       preprocessing.remove_diacritics,
+                       preprocessing.remove_stopwords,
+                       preprocessing.remove_whitespace
+                      ]
+
+    # Pass the custom_pipeline to the pipeline argument
+    cleantext = hero.clean(dat[0], pipeline=custom_pipeline)
+
+    # Transform the preprocessed text using the loaded vectorizer
+    X = vectorizer.transform(cleantext)
+    return X.toarray()
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Get the port from the environment variable or use 5000 by default
+    app.run(host='0.0.0.0', port=port)
+
+
+
+# In[ ]:
+
+
+
+
